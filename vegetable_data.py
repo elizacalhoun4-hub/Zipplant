949d4697cce6eef1b77f0bdaaa61bf2d60e410bb
elizacalhoun4-hub <elizacalhoun4@gmail.com>
2026-05-06 03:18:54 -0500

    Create vegetable_data.py

---
+++ vegetable_data.py
@@ -0,0 +1,17 @@
+# vegetable_data.py
+plants = [
+    {
+        "name": "Corn",
+        "cultivar": "Golden Bantam",
+        "type": "Heirloom",
+        "origin": "Traditional American",
+        "days_to_maturity": 80
+    },
+    {
+        "name": "Bok Choy",
+        "cultivar": "Joi Choi",
+        "type": "Hybrid",
+        "origin": "Asian",
+        "days_to_maturity": 45
+    }
+]
