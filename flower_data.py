862866c216e53416183ae9064a4916d20a951663
elizacalhoun4-hub <elizacalhoun4@gmail.com>
2026-05-06 03:20:21 -0500

    Create flower_data.py

---
+++ flower_data.py
@@ -0,0 +1,17 @@
+# flower_data.py
+plants = [
+    {
+        "name": "Marigold",
+        "cultivar": "Crackerjack",
+        "type": "Heirloom",
+        "flower_type": "African",
+        "pollinator_friendly": True
+    },
+    {
+        "name": "Zinnia",
+        "cultivar": "Profusion",
+        "type": "Hybrid",
+        "flower_type": "Dwarf/Mounding",
+        "pollinator_friendly": True
+    }
+]
